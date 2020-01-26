51f52e5306543a57d92fa2cd34319d38819996d4
harunamarun <harunamarun@gmail.com>
2020-01-26 15:00:04 +0900

    displayed todo list

--- public/js/todo_list.js
+++ public/js/todo_list.js
@@ -1,5 +1,32 @@
 async function showList() {
   const request = await fetch("http://localhost:3000/api/todos");
   const todos = await request.json();
+  let latest = todos.length - 1;
+  let index = 0;
+  while (index <= latest) {
+    const post = todos[index];
+
+    const titleEl = document.createElement("div");
+    titleEl.className = "title";
+    titleEl.innerText = post.title;
+
+    const duedateEl = document.createElement("div");
+    duedateEl.className = "duedate";
+    duedateEl.innerText = post.dueDate.split(" ")[0];
+
+    const priopiryEl = document.createElement("div");
+    priopiryEl.className = "priority";
+    priopiryEl.innerText = post.priority;
+
+    const containerEl = document.getElementById("todo_list");
+    const todoEl = document.createElement("div");
+    todoEl.className = "todo";
+    containerEl.appendChild(todoEl);
+    todoEl.appendChild(titleEl);
+    todoEl.appendChild(duedateEl);
+    todoEl.appendChild(priopiryEl);
+
+    index += 1;
+  }
 }
 showList();
