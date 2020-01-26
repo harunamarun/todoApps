d126b6410b4793fc4ff0917b0ece9a7a49b51ff3
harunamarun <harunamarun@gmail.com>
2020-01-26 17:50:46 +0900

    fixed to delete todo

--- public/js/todo_detail.js
+++ public/js/todo_detail.js
@@ -1,9 +1,36 @@
 console.log("location.pathname", location.pathname);
 const patharr = location.pathname.split("/");
 const todoId = patharr[patharr.length - 1];
+
 async function getTodo() {
   const request = await fetch(`http://localhost:3000/api/todos/${todoId}`);
-  const jsons = await request.json();
-  console.log("jsons", jsons);
+  const todo = await request.json();
+  document.getElementById("title-text").value = todo.title;
+  document.getElementById("description-text").value = todo.description;
+  document.getElementById("due-date").value = todo.dueDate.split(" ")[0];
+  document.getElementById("priority-select").value + todo.priority;
 }
 getTodo();
+
+async function saveTodo() {
+  const newPost = {
+    title: document.getElementById("title-text").value,
+    description: document.getElementById("description-text").value,
+    dueDate: document.getElementById("due-date").value,
+    priority: document.getElementById("priority-select").value
+  };
+  fetch(`http://localhost:3000/api/todos/${todoId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    },
+    body: JSON.stringify(newPost)
+  })
+    .then(() => (window.location.href = "/todos"))
+    .catch(error => console.error(error));
+}
+
+window.onload = () => {
+  let saveBtn = document.getElementById("save-btn");
+  saveBtn.addEventListener("click", saveTodo);
+};
--- public/js/todo_list.js
+++ public/js/todo_list.js
@@ -18,15 +18,67 @@ async function showList() {
     priopiryEl.className = "priority";
     priopiryEl.innerText = post.priority;
 
+    const todoCard = document.createElement("a");
+    todoCard.setAttribute("href", `/todos/${post.id}`);
+    todoCard.appendChild(titleEl);
+    todoCard.appendChild(duedateEl);
+    todoCard.appendChild(priopiryEl);
+    todoCard.className = "todo-card";
+
+    const todoDeleteBtn = document.createElement("button");
+    todoDeleteBtn.className = "todo-delete-btn";
+    todoDeleteBtn.innerText = "delete";
+    todoDeleteBtn.id = post.id;
+
+    const todoContainer = document.createElement("div");
+    todoContainer.className = "todo-container";
+    todoContainer.appendChild(todoCard);
+    todoContainer.appendChild(todoDeleteBtn);
+
     const containerEl = document.getElementById("todo_list");
-    const todoEl = document.createElement("div");
-    todoEl.className = "todo";
-    containerEl.appendChild(todoEl);
-    todoEl.appendChild(titleEl);
-    todoEl.appendChild(duedateEl);
-    todoEl.appendChild(priopiryEl);
+    containerEl.appendChild(todoContainer);
 
     index += 1;
   }
 }
 showList();
+
+async function deleteTodo() {
+  fetch(`http://localhost:3000/api/todos/${this.id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    }
+  })
+    .then(() => location.reload())
+    .catch(error => console.error(error));
+}
+
+async function addTodo() {
+  const newPost = {
+    title: document.getElementById("title-text").value,
+    description: document.getElementById("description-text").value,
+    dueDate: document.getElementById("due-date").value,
+    priority: document.getElementById("priority-select").value
+  };
+  fetch(`http://localhost:3000/api/todos`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    },
+    body: JSON.stringify(newPost)
+  })
+    .then(() => location.reload())
+    .then(response => response.json())
+    .then(data => JSON.stringify(data))
+    .catch(error => console.error(error));
+}
+
+window.onload = () => {
+  let postBtn = document.getElementById("post-btn");
+  postBtn.addEventListener("click", addTodo);
+  let deleteBtns = document.getElementsByClassName("todo-delete-btn");
+  for (const btn of deleteBtns) {
+    btn.addEventListener("click", deleteTodo);
+  }
+};
